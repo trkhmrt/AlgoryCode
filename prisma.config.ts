1257c5419f7c1ba5cffbf87119134f66c1f822e0
trkhmrt <trkhamarat@gmail.com>
2026-07-02 02:30:32 +0300

    Fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import { defineConfig, env } from "prisma/config";
 
 config({ path: ".env.local" });
+config({ path: ".env" });
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -9,6 +10,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DIRECT_URL ?? env("DATABASE_URL"),
+    url: process.env.DIRECT_URL ?? process.env.DATABASE_URL ?? env("DATABASE_URL"),
   },
 });
